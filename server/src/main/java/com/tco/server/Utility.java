27f498fac6ca5804b2f093110170ae452d8b5f6d
kgriem <kgriem@rams.colostate.edu>
2020-03-11 13:43:39 -0600

    a less broken distance function

--- server/src/main/java/com/tco/server/Utility.java
+++ server/src/main/java/com/tco/server/Utility.java
@@ -4,6 +4,10 @@ public class Utility {
 
     double getDistance(double long1, double lat1, double long2, double lat2, int earthradius) {
         double f = 1 / 298.257223563;
+        //System.out.println("long "+long1);
+        //System.out.println("lat "+lat1);
+        //System.out.println("long "+long2);
+        //System.out.println("lat "+lat2);
         double DL = Math.toRadians(long2 - long1);
         double U1 = Math.atan((1 - f) * Math.tan(Math.toRadians(lat1)));
         double U2 = Math.atan((1 - f) * Math.tan(Math.toRadians(lat2)));
@@ -15,8 +19,7 @@ public class Utility {
         double sinDL = Math.sin(DL);
         double top = Math.sqrt(Math.pow(cosU2 * sinDL, 2) + Math.pow(cosU1 * sinU2 - sinU1 * cosU2 * cosDL, 2));
         double bottom = sinU1 * sinU2 + cosU1 * cosU2 * cosDL;
-        double distance = Math.atan2(top , bottom) * earthradius;
-        return distance;
+        double distance = Math.atan2(top, 1 / bottom) * earthradius;
+        return Math.round(distance);
     }
-
 }
