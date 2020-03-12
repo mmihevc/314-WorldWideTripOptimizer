95fe9be9bda44fef85d97b1263d0265080bd27ce
Kevin Schroeder <kmschr@gmail.com>
2020-03-12 13:11:18 -0600

    Created component for adding destinations

--- client/src/components/Atlas/Atlas.js
+++ client/src/components/Atlas/Atlas.js
@@ -44,7 +44,7 @@ export default class Atlas extends Component {
             isSubmit: [],
             userMarkers: [],
             markerArray : [],
-            numDestinations: 3
+            numDestinations: 1
         };
 
         let i;
@@ -63,6 +63,7 @@ export default class Atlas extends Component {
                             {this.renderLeafletMap()}
                             {this.renderHomeButton()}
                             {this.renderMultiple(this.state.numDestinations, this.renderLongitudeLatitudeBox)}
+                            {this.renderAddDestinationButton()}
                         </Col>
                     </Row>
                 </Container>
@@ -167,6 +168,15 @@ export default class Atlas extends Component {
                     <Button onClick={() => this.handleInputChange(index)}>Submit</Button>
                 </InputGroup>
             </Form>
+        )
+    }
+
+    renderAddDestinationButton() {
+        return (
+            <Button title="Add Destination" className="mt-1"
+                    onClick={() => {}}>
+                +
+            </Button>
         )
     }
 
