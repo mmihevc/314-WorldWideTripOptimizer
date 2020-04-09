55532a9b6ae10d66aaee39e7ea05783349bc92cf
Kevin Schroeder <kmschr@gmail.com>
2020-04-09 15:22:30 -0600

    Build and test distance matrix
    also reverting some unreverted commits to master

--- server/src/test/java/com/tco/server/TestNearestNeighbor.java
+++ server/src/test/java/com/tco/server/TestNearestNeighbor.java
@@ -1,14 +1,10 @@
 package com.tco.server;
 
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import static org.junit.Assert.*;
 
 public class TestNearestNeighbor {
-
-    private final Logger log = LoggerFactory.getLogger(TestNearestNeighbor.class);
 
     @Test
     public void testBuildDistanceMatrix() {
