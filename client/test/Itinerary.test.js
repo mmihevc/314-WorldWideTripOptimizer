3f40006203102d5c17b64eacd6adcd4ef2a32e49
Kevin Schroeder <kmschr@gmail.com>
2020-04-15 18:10:59 -0600

    improved coverage in testItinerary

--- client/test/Itinerary.test.js
+++ client/test/Itinerary.test.js
@@ -6,7 +6,12 @@ import Itinerary from '../src/components/Atlas/Itinerary';
 import {Table, Button} from "reactstrap";
 
 function testItinerary() {
-    const itinerary = shallow(<Itinerary destinations={[{name: "test", lat:0, lng:0}]}/>);
+    const itinerary = shallow(<Itinerary destinations={[
+        {name: "test1", lat:0, lng:0},
+        {name: "test2", lat:7, lng:8},
+        {name: "test3", lat:12, lng:-4},
+        {name: "test4", lat:5, lng:20},
+        ]}/>);
     expect(itinerary.find(Table).length).toBe(0);
     simulateOnClick(itinerary.find(Button), itinerary);
     expect(itinerary.find(Table).length).toBe(1);
