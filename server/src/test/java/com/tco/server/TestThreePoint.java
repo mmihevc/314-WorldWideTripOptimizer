639cfba9d4f64701353d27cb584532f723683034
Kevin Schroeder <kmschr@gmail.com>
2020-04-24 02:20:02 -0600

    3opt works now

--- server/src/test/java/com/tco/server/TestThreePoint.java
+++ server/src/test/java/com/tco/server/TestThreePoint.java
@@ -4,31 +4,40 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static org.junit.Assert.assertArrayEquals;
+
 public class TestThreePoint {
     private final static Logger log = LoggerFactory.getLogger(twoPointOptimization.class);
 
-    public void testTwoPoint(){
-        Place[] atrip = new Place[6];
-        Place[] btrip = new Place[6];
-        atrip[0] = new Place("45.415498", "6.634682", "Courchevel Tourisme");
-        atrip[1] = new Place("47.137390", "10.269852", "St Anton am Arlberg");
-        atrip[2] = new Place("40.635036", "-111.476452", "Deer Valley Resort");
-        atrip[3] = new Place("50.115088", "-122.948647", "Whistler Blackcomb");
-        atrip[4] = new Place("45.933915", "7.629187", "Cervinia Valtournenche");
-        atrip[5] = new Place("-37.124956", "146.447541", "Mount Buller");
-        btrip[0]=atrip[0];
-        btrip[1]=atrip[2];
-        btrip[2]=atrip[3];
-        btrip[3]=atrip[4];
-        btrip[4]=atrip[5];
-        btrip[5]=atrip[1];
-        threePointOptimization.optimize(atrip);
+    @Test
+    public void testTwoPoint() {
+        Place[] places = new Place[6];
+        Place[] expected = new Place[6];
+        places[0] = new Place("39.7392", "-104.9903", "Denver");
+        places[1] = new Place("40.48", "-104.91", "Windsor");
+        places[2] = new Place("40.01499", "-105.27055", "Boulder");
+        places[3] = new Place("40.585258", "-105.084419", "Fort Collins");
+        places[4] = new Place("39.76", "-105.23", "Golden");
+        places[5] = new Place("40.41", "-104.71", "Greeley");
+        expected[0]=places[0];
+        expected[1]=places[4];
+        expected[2]=places[2];
+        expected[3]=places[5];
+        expected[4]=places[1];
+        expected[5]=places[3];
+        threePointOptimization.optimize(places);
+        assertArrayEquals("msg", places, expected);
+    }
 
-        for (Place place: atrip) {
-            log.error(place.name);
-        }
-
-        //assertArrayEquals("msg", atrip, btrip);
+    @Test
+    public void testSwap() {
+        int[] route = {5, 6, 7, 8, 9, 10, 11, 12, 17};
+        int[] swappedRoute = {5, 9, 10, 11, 12, 6, 7, 8, 17};
+        int i = 0;
+        int j = 3;
+        int k = 7;
+        threePointOptimization.swap(route, i+1, j, k);
+        assertArrayEquals("3-opt swap", swappedRoute, route);
     }
 
 }
