fde86f0ad4d2992847c30b34509a00bf406ca1c6
Maddie <maddie@mihevc.com>
2020-04-30 15:33:55 -0600

    adding findCall and updating MicroServer

---
+++ client/src/utils/findCall.js
@@ -0,0 +1,28 @@
+import {HTTP_OK, PROTOCOL_VERSION} from "../components/Constants";
+import * as findSchema from "../../schemas/FindResponse";
+import {isJsonResponseValid, sendServerRequestWithBody} from "./restfulAPI";
+
+export function findCall(match, limit, type, where, found = 1, port) {
+    let values = {
+        requestVersion: PROTOCOL_VERSION,
+        requestType: 'find',
+        match: match,
+        narrow: [],
+        limit: limit,
+        found: found,
+        places: []
+    }
+    sendServerRequestWithBody('find', values, port).then(
+        aTrip => processFindResponse(aTrip)
+    )
+}
+
+function processFindResponse(aTrip, callback){
+    if (!isJsonResponseValid(aTrip.body, findSchema)){
+        alert('error fetching find')
+    }
+    else if (aTrip.statusCode === HTTP_OK){
+        callback(aTrip.body)
+        return aTrip;
+    }
+}
