e22f80c7394f510ce0246d0010f11c79dd9d9ba6
Kellyn Dassler <kellyndassler@gmail.com>
2020-04-30 19:02:28 -0600

    delete start box for delete all button

--- client/src/components/Atlas/Atlas.js
+++ client/src/components/Atlas/Atlas.js
@@ -141,6 +141,8 @@ export default class Atlas extends Component {
             //this.setState({numInputs: this.state.numInputs-1, this.handleInputChange});
             //this.handleInputChange();
         this.setState({numInputs: 0}, this.handleInputChange);
+        this.displayStartBox();
+       // this.setState({showStartBox: !this.state.showStartBox})
         // clear itinerary too?
     }
 
